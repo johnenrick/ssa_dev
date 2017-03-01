e7846f846769db6af10d9e60b743819805f1cc98
johnenrick <plenosjohn@yahoo.com>
2017-03-01 10:40:31 +0800

    db bu

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = 'abcdefg';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 36000;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
